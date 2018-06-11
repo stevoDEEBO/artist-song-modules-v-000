773cfee4f36b294f7ab4b1d0a87e4b61bbee8433
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-11 06:35:04 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,18 @@
+module Memorable
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+
+  module ClassMethods
+    def count
+      self.all.count
+    end
+
+    def reset_all
+      self.all.clear
+    end
+  end
+end
